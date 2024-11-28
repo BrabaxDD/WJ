28d8a1910c4a4a808af3e3cbc16c4dfca8ef24ed
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2024-11-28 16:28:04 +0100

    did nothing

--- Simulation/results/H2O.tmol OHDefect.tmol/script.py
+++ Simulation/results/H2O.tmol OHDefect.tmol/script.py
@@ -1,5 +1,5 @@
-from ase.io import read,write
-import os 
+from ase.io import read, write
+import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 gas = read("gas")
 defect = read("defect")
@@ -7,5 +7,4 @@ print("-------------------------------------------------------------------------
 print("calculating: " + dir_path)
 print(gas.get_positions())
 interaction = gas + defect
-interaction.write("interaction",format="turbomole")
-
+interaction.write("interaction", format="turbomole")
