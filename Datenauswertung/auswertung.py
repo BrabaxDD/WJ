413b4de8bf62e3d337884c8fcdd4e20ee4c2a628
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2025-03-03 10:49:35 +0100

    Messereihen Bilder

--- Datenauswertung/auswertung.py
+++ Datenauswertung/auswertung.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 data = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [],
         10: [], 11: [], 12: [], 12: [], 13: [], 14: [], 15: [], 16: [],}
 
-with open("amoniummessung.data", "r") as file:
+with open("20250225_SH2_2.dat", "r") as file:
     for line in file:
         measurments = line.strip().split()
         for i, measurment in enumerate(measurments):
@@ -17,21 +17,24 @@ with open("amoniummessung.data", "r") as file:
 #print(data[1])
 toplotRelative = []
 toplotDiffrence = []
+toplotAbsolute = []
 df = pd.DataFrame(data)
 for i,measurement in enumerate(df):
     if i != 0 and i not in [1,2,3,4,9,14,15,16]:
 
     #if i != 0 and i not in [1,2,3,14,16]:
-        origin = df[i][:1000].mean()
-        df["rolling" + str(i)] = df[i].rolling(window=100).mean()
+        origin = df[i][:2000].mean()
+        df["rolling" + str(i)] = df[i].rolling(window=800).mean()
         df["rollingRelative" + str(i)] = df["rolling" + str(i)] / origin
         df["rollingDiffrence"+str(i)] = df["rolling"+str(i)] - origin
         toplotRelative.append("rollingRelative" + str(i))
         toplotDiffrence.append("rollingDiffrence" + str(i))
+        toplotAbsolute.append("rolling"+str(i))
 #        df.plot(x=0,y=("rollingRelative" + str(i)), kind = "line")
 
-df.plot(x = 0,y=toplotRelative,kind="line")
-df.plot(x = 0,y=toplotDiffrence,kind="line")
+df.plot(x = 0,y=toplotRelative,kind="line",xlabel="Zeit in Millisekunden",ylabel="relativer Widerstand")
+df.plot(x = 0,y=toplotDiffrence,kind="line",xlabel="Zeit in Millisekunden",ylabel="Widerstands Different in Ohm")
+df.plot(x = 0,y=toplotAbsolute,kind="line",xlabel="Zeit in Millisekunden",ylabel="Widerstand in Ohm")
 
 
 plt.show()
